81db778336debd5ac0351ca3bc0e280b7e64c308
vineetsaini007 <vineetsainiii007@gmail.com>
2026-06-24 18:44:37 +0530

    set up Express server with initial routes and middleware

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const productRoutes = require(
+  "./routes/productRoutes"
+);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API Running",
+  });
+});
+
+app.use(
+  "/api/products",
+  productRoutes
+);
+
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
+});
